f3b484ae256af9510b360f65c439c90fec2f42b4
Jason Goldfine-Middleton <jrgoldfinemiddleton@gmail.com>
2017-07-14 05:00:56 +0000

    Create sudo user role

---
+++ ansible/create_role.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env sh
+
+BASEDIR="$(cd "$(dirname "$0")"; pwd)"
+#echo "$BASEDIR"
+
+cd "$BASEDIR"
+
+if [ -z $1 ]; then
+#	echo "usage: $0 role_name"
+	exit 1
+fi
+
+if ! [ -d "roles/$1" ]; then
+#	echo "Creating directory '${BASEDIR}/roles/$1'..."
+	mkdir -p "roles/$1"
+fi
+
+cd "roles/$1"
+
+for i in defaults files handlers meta tasks templates vars; do
+	if ! [ -d "$i" ]; then
+#		echo "Creating directory '${BASEDIR}/roles/$1/$i'..."
+		mkdir "$i"
+	fi
+
+	if [ $i != files -a $i != templates ]; then
+#		echo "Creating file '${BASEDIR}/roles/$1/$i/main.yml'..."
+		echo '---' > "$i/main.yml"
+	fi
+done
